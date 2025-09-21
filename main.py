a011e411b717735c11e35519b9c1639d8476e67f
Solipsismo <yuzcrisllan2@gmail.com>
2025-09-21 15:29:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,19 @@
 import os
-
-def deletar_lista(lista:str):
-    try:
-        os.remove(lista)
-    except FileNotFoundError:
-        print(f"Arquivo '{lista}' não encontrado.")
+import lista_func as L
 
 def limpar_tela():
     os.system('clear')
 
-def inserir_afazer():
-    afazer=str(input("Digite o afazer: \n"))
+def inserir_a_fazer():
+    afazer=str(input("Digite o a fazer: \n"))
     return afazer
-
-class Lista:
-    def __init__(self):
-        self.itens = []
-
-    def salvar_lista(self):
-        with open('lista.txt', 'w') as l:
-            for item in self.itens:
-                l.write(f"{item}\n")
-
-    def carregar_lista(self):
-        try:
-            with open('lista.txt', 'r') as f:
-                 linhas = [linha.strip() for linha in f]
-                 for x in linhas:
-                     self.itens.append(x)
-        except FileNotFoundError:
-            pass
-
-    def adicionar_item(self, item:str):
-        self.itens.append(item)
-
-    def editar_item(self,num_lista:int,novo_item:str):
-        num_lista-=1
-        self.itens[num_lista]=novo_item
-
-    def remover_item(self, item:int):
-        item=item-1
-        self.itens.remove(self.itens[item])
-
-    def mostrar_lista(self):
-        for x in range(len(self.itens)):
-            print(f"{x+1} - {self.itens[x]}")
 
 def Mostrar_menu():
     limpar_tela()
     print("=========   LISTA DE AFAZERES   =========\n")
     Lista1.mostrar_lista()
 
-Lista1=Lista()
+Lista1=L.Lista()
 Lista1.carregar_lista()
 
 def menu():
@@ -60,7 +22,7 @@ def menu():
         Mostrar_menu()
         if erro_menu:
             print(f"\n{erro_menu}")
-        operacao = input("\nDigite a operação desejada:\n1 - Adicionar afazer\n2 - Remover afazer\n3 - Editar\n4 - Sair\n")
+        operacao = input("\nDigite a operação desejada:\n1 - Adicionar a fazer\n2 - Remover a fazer\n3 - Editar\n4 - Sair\n")
         try:
             operacao = int(operacao)
             if operacao in [1, 2, 3, 4]:
@@ -72,9 +34,9 @@ def menu():
 
     match operacao:
         case 1:
-            item = inserir_afazer()
+            item = inserir_a_fazer()
             Lista1.adicionar_item(item)
-            deletar_lista("lista.txt")
+            L.deletar_lista("lista.txt")
             Lista1.salvar_lista()
         case 2:
             erro_remover = ""
@@ -88,7 +50,7 @@ def menu():
                         break
                     item_remover = int(item_remover)
                     Lista1.remover_item(item_remover)
-                    deletar_lista("lista.txt")
+                    L.deletar_lista("lista.txt")
                     Lista1.salvar_lista()
                     break
                 except IndexError:
@@ -106,9 +68,9 @@ def menu():
                     if item_editar == "X":
                         break
                     item_editar = int(item_editar)
-                    novo_item = input("Digite o novo afazer:\n")
+                    novo_item = input("Digite o novo a fazer:\n")
                     Lista1.editar_item(item_editar, novo_item)
-                    deletar_lista("lista.txt")
+                    L.deletar_lista("lista.txt")
                     Lista1.salvar_lista()
                     break
                 except IndexError:
@@ -116,7 +78,7 @@ def menu():
                 except ValueError:
                     erro_editar = "Digite um número válido"
         case 4:
-            deletar_lista("lista.txt")
+            L.deletar_lista("lista.txt")
             Lista1.salvar_lista()
             exit()
 
